465c7848d40c66bafe87390e375cd89fa2de92d1
Yin <yinnnn17@gmail.com>
2021-10-01 19:21:00 -0700

    [update] finished Serializing & Saving

--- scrape.go
+++ scrape.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"encoding/json"
+	"io/ioutil"
+	"log"
 	"os"
 
 	"github.com/gocolly/colly"
@@ -10,40 +12,39 @@ import (
 type foodRecipe struct {
 	Title string `json:"title"`
 	Img   string `json:"img"`
-	Link  string `json:"href"`
+	Href  string `json:"href"`
 }
 
 func main() {
+	var recipes []foodRecipe
 	c := colly.NewCollector()
-
 	c.OnHTML("article.category-taiwanese", func(e *colly.HTMLElement) {
-		link := e.Attr("href")
+		href := e.ChildAttr("a", "href")
 		img := e.ChildAttr("img", "src")
 		title := e.ChildText("h3")
-		print(link, img, title)
 
-		foodRecipe := foodRecipe{
+		recipes = append(recipes, foodRecipe{
 			Title: title,
 			Img:   img,
-			Link:  link,
-		}
-		foodRecipeJson, _ := json.Marshal(foodRecipe)
-
-		f, err := os.OpenFile("file.txt", os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0600)
-		if err != nil {
-			panic(err)
-		}
-
-		defer f.Close()
-
-		foodRecipeStr := string(foodRecipeJson)
-
-		if _, err = f.WriteString(foodRecipeStr); err != nil {
-			panic(err)
-		}
-
+			Href:  href,
+		})
 	})
-
 	c.Visit("https://chejorge.com/")
+
+	foodRecipeJson, _ := json.MarshalIndent(recipes, "", " ")
+
+	print(string(foodRecipeJson))
+
+	// writing json to file
+	_ = ioutil.WriteFile("output.json", foodRecipeJson, 0644)
+
+	// to append to a file
+	// create the file if it doesn't exists with O_CREATE, Set the file up for read write, add the append flag and set the permission
+	f, err := os.OpenFile("./debug-web.log", os.O_CREATE|os.O_RDWR|os.O_APPEND, 0660)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// write to file, f.Write()
+	f.Write(foodRecipeJson)
 
 }
